95f5d80328b35681669cee08e2b69dd28a6824bd
Yiseul Kim <yiseulkim@Yiseului-MacBookPro.local>
2023-08-08 11:31:50 +0900

    style:cog아이콘 수정

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,9 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import PropTypes from 'prop-types';
-import { withRouter } from 'react-router';
+import React from "react";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import { withRouter } from "react-router";
+import { FaCog } from "react-icons/fa";
+
 import {
   Navbar,
   Nav,
@@ -17,26 +19,31 @@ import {
   Input,
   Form,
   FormGroup,
-} from 'reactstrap';
-import cx from 'classnames';
-import { NavbarTypes } from '../../reducers/layout';
-import Notifications from '../Notifications';
-import { logoutUser } from '../../actions/auth';
-import Joyride, { STATUS } from 'react-joyride';
-import { toggleSidebar, openSidebar, closeSidebar, changeActiveSidebarItem } from '../../actions/navigation';
+} from "reactstrap";
+import cx from "classnames";
+import { NavbarTypes } from "../../reducers/layout";
+import Notifications from "../Notifications";
+import { logoutUser } from "../../actions/auth";
+import Joyride, { STATUS } from "react-joyride";
+import {
+  toggleSidebar,
+  openSidebar,
+  closeSidebar,
+  changeActiveSidebarItem,
+} from "../../actions/navigation";
 
-import adminDefault from '../../images/chat/chat2.png';
-import MenuIcon from '../../images/sidebar/Fill/MenuIcon';
-import FlipIcon from '../../images/sidebar/Outline/Flip';
-import CloseIcon from '../../images/sidebar/Fill/CloseIconOne';
-import SearchIcon from '../../images/sidebar/Outline/Search';
-import SettingsIcon from '../../images/sidebar/Outline/Settings';
-import CalendarIcon from '../../images/sidebar/Outline/Calendar';
-import PersonIcon from '../../images/sidebar/Outline/Person';
-import EmailIcon from '../../images/sidebar/Outline/Email';
-import PowerIcon from '../../images/sidebar/Outline/Power';
+import adminDefault from "../../images/chat/chat2.png";
+import MenuIcon from "../../images/sidebar/Fill/MenuIcon";
+import FlipIcon from "../../images/sidebar/Outline/Flip";
+import CloseIcon from "../../images/sidebar/Fill/CloseIconOne";
+import SearchIcon from "../../images/sidebar/Outline/Search";
+import SettingsIcon from "../../images/sidebar/Outline/Settings";
+import CalendarIcon from "../../images/sidebar/Outline/Calendar";
+import PersonIcon from "../../images/sidebar/Outline/Person";
+import EmailIcon from "../../images/sidebar/Outline/Email";
+import PowerIcon from "../../images/sidebar/Outline/Power";
 
-import s from './Header.module.scss'; // eslint-disable-line css-modules/no-unused-class
+import s from "./Header.module.scss"; // eslint-disable-line css-modules/no-unused-class
 
 class Header extends React.Component {
   static propTypes = {
@@ -67,44 +74,45 @@ class Header extends React.Component {
       run: false,
       steps: [
         {
-          content: 'You can adjust sidebar, or leave it closed 😃',
-          placement: 'bottom',
-          target: '#toggleSidebar',
-          textAlign: 'center',
-          disableBeacon: true
+          content: "You can adjust sidebar, or leave it closed 😃",
+          placement: "bottom",
+          target: "#toggleSidebar",
+          textAlign: "center",
+          disableBeacon: true,
         },
         {
           content: "Admin can check out his messages and tasks easily 😃",
-          placement: 'bottom',
-          target: '.dropdown-toggle',
+          placement: "bottom",
+          target: ".dropdown-toggle",
         },
         {
-          content: "Clickable cog can provide you with link to important pages 😄",
-          placement: 'bottom',
-          target: '.tutorial-dropdown',
+          content:
+            "Clickable cog can provide you with link to important pages 😄",
+          placement: "bottom",
+          target: ".tutorial-dropdown",
         },
         {
-          content: 'Open theme cusomizer sidebar, play with it or watch tour! ❤️',
-          placement: 'left',
-          target: '.helper-button'
+          content:
+            "Open theme cusomizer sidebar, play with it or watch tour! ❤️",
+          placement: "left",
+          target: ".helper-button",
         },
       ],
     };
   }
 
   componentDidMount() {
-    if (window.location.href.includes('main')) {
-      this.setState({ run: true })
+    if (window.location.href.includes("main")) {
+      this.setState({ run: true });
     }
   }
 
   handleJoyrideCallback = (CallBackProps) => {
     const { status } = CallBackProps;
 
-    if (([STATUS.FINISHED, STATUS.SKIPPED]).includes(status)) {
+    if ([STATUS.FINISHED, STATUS.SKIPPED].includes(status)) {
       this.setState({ run: false });
     }
-
   };
 
   start = () => {
@@ -114,8 +122,8 @@ class Header extends React.Component {
   };
 
   toggleFocus = () => {
-    this.setState({ focus: !this.state.focus })
-  }
+    this.setState({ focus: !this.state.focus });
+  };
 
   toggleNotifications() {
     this.setState({
@@ -133,10 +141,10 @@ class Header extends React.Component {
       this.props.dispatch(closeSidebar());
       this.props.dispatch(changeActiveSidebarItem(null));
     } else {
-      const paths = this.props.location.pathname.split('/');
+      const paths = this.props.location.pathname.split("/");
       paths.pop();
       this.props.dispatch(openSidebar());
-      this.props.dispatch(changeActiveSidebarItem(paths.join('/')));
+      this.props.dispatch(changeActiveSidebarItem(paths.join("/")));
     }
   }
 
@@ -144,13 +152,13 @@ class Header extends React.Component {
   toggleSidebar() {
     this.props.dispatch(toggleSidebar());
     if (this.props.sidebarStatic) {
-      localStorage.setItem('staticSidebar', 'false');
+      localStorage.setItem("staticSidebar", "false");
       this.props.dispatch(changeActiveSidebarItem(null));
     } else {
-      localStorage.setItem('staticSidebar', 'true');
-      const paths = this.props.location.pathname.split('/');
+      localStorage.setItem("staticSidebar", "true");
+      const paths = this.props.location.pathname.split("/");
       paths.pop();
-      this.props.dispatch(changeActiveSidebarItem(paths.join('/')));
+      this.props.dispatch(changeActiveSidebarItem(paths.join("/")));
     }
   }
 
@@ -162,15 +170,22 @@ class Header extends React.Component {
   render() {
     const { focus } = this.state;
     const { openUsersList } = this.props;
-    const navbarType = localStorage.getItem("navbarType") || 'static'
+    const navbarType = localStorage.getItem("navbarType") || "static";
 
     const user = this.props.currentUser;
-    const avatar = user && user.avatar && user.avatar.length && user.avatar[0].publicUrl;
+    const avatar =
+      user && user.avatar && user.avatar.length && user.avatar[0].publicUrl;
 
-    const firstUserLetter = user && (user.firstName|| user.email)[0].toUpperCase();
+    const firstUserLetter =
+      user && (user.firstName || user.email)[0].toUpperCase();
 
     return (
-      <Navbar className={`${s.root} d-print-none ${navbarType === NavbarTypes.FLOATING ? s.navbarFloatingType : ''}`}  style={{zIndex: !openUsersList ? 100 : 0}}>
+      <Navbar
+        className={`${s.root} d-print-none ${
+          navbarType === NavbarTypes.FLOATING ? s.navbarFloatingType : ""
+        }`}
+        style={{ zIndex: !openUsersList ? 100 : 0 }}
+      >
         <Joyride
           callback={this.handleJoyrideCallback}
           continuous={true}
@@ -182,11 +197,11 @@ class Header extends React.Component {
           disableScrolling
           styles={{
             options: {
-              arrowColor: '#ffffff',
-              backgroundColor: '#ffffff',
-              overlayColor: 'rgba(79, 26, 0, 0.4)',
-              primaryColor: '#000',
-              textColor: '#495057',
+              arrowColor: "#ffffff",
+              backgroundColor: "#ffffff",
+              overlayColor: "rgba(79, 26, 0, 0.4)",
+              primaryColor: "#000",
+              textColor: "#495057",
               spotlightPadding: 0,
               zIndex: 1000,
               padding: 5,
@@ -197,15 +212,15 @@ class Header extends React.Component {
               padding: 15,
             },
             tooltipContent: {
-              padding: '20px 5px 0',
+              padding: "20px 5px 0",
             },
             floater: {
               arrow: {
-                padding: 10
+                padding: 10,
               },
             },
             buttonClose: {
-              display: 'none'
+              display: "none",
             },
             buttonNext: {
               backgroundColor: "#21AE8C",
@@ -213,11 +228,11 @@ class Header extends React.Component {
               borderRadius: 4,
               color: "#ffffff",
               fontWeight: "bold",
-              outline: "none"
+              outline: "none",
             },
             buttonBack: {
               color: "#798892",
-              marginLeft: 'auto',
+              marginLeft: "auto",
               fontSize: 13,
               marginRight: 5,
             },
@@ -229,24 +244,33 @@ class Header extends React.Component {
         />
         <Nav>
           <NavItem>
-            <NavLink className={`d-md-down-none ${s.toggleSidebar}`} id="toggleSidebar" onClick={this.toggleSidebar}>
+            <NavLink
+              className={`d-md-down-none ${s.toggleSidebar}`}
+              id="toggleSidebar"
+              onClick={this.toggleSidebar}
+            >
               <span className={s.headerSvgFlipColor}>
-                <MenuIcon  maskId={1001}/>
+                <MenuIcon maskId={1001} />
               </span>
             </NavLink>
             <UncontrolledTooltip placement="bottom" target="toggleSidebar">
-              Turn on/off<br />sidebar<br />collapsing
+              Turn on/off
+              <br />
+              sidebar
+              <br />
+              collapsing
             </UncontrolledTooltip>
             <NavLink className="fs-lg d-lg-none" onClick={this.switchSidebar}>
-            <span 
-              className={`rounded rounded-lg d-md-none d-sm-down-block`} style={{marginTop: 7}}>
-                <span 
-                  className={s.headerSvgFlipColor}
-                  style={{fontSize: 30}} 
-                ><MenuIcon maskId={1000}/></span>
+              <span
+                className={`rounded rounded-lg d-md-none d-sm-down-block`}
+                style={{ marginTop: 7 }}
+              >
+                <span className={s.headerSvgFlipColor} style={{ fontSize: 30 }}>
+                  <MenuIcon maskId={1000} />
+                </span>
               </span>
               <span className={`ml-3 d-sm-down-none ${s.headerSvgFlipColor}`}>
-                <MenuIcon maskId={999}/>
+                <MenuIcon maskId={999} />
               </span>
             </NavLink>
           </NavItem>
@@ -264,63 +288,129 @@ class Header extends React.Component {
               </span>
             </NavLink>
           </NavItem>
-
         </Nav>
 
         <Form className={`d-sm-down-none ml-5 ${s.headerSearchInput}`} inline>
           <FormGroup>
-            <InputGroup onFocus={this.toggleFocus} onBlur={this.toggleFocus} className={
-              cx('input-group-no-border', {'focus' : !!focus})
-            }>
+            <InputGroup
+              onFocus={this.toggleFocus}
+              onBlur={this.toggleFocus}
+              className={cx("input-group-no-border", { focus: !!focus })}
+            >
               <InputGroupAddon addonType="prepend">
-                <span className={`${s.headerSvgFlipColor}`}><SearchIcon /></span>
+                <span className={`${s.headerSvgFlipColor}`}>
+                  <SearchIcon />
+                </span>
               </InputGroupAddon>
-              <Input id="search-input" placeholder="Search Dashboard" className={cx({'focus' : !!focus})} />
+              <Input
+                id="search-input"
+                placeholder="Search Dashboard"
+                className={cx({ focus: !!focus })}
+              />
             </InputGroup>
           </FormGroup>
         </Form>
 
-        <NavLink className={`${s.navbarBrand} d-md-none ${s.headerSvgFlipColor}`}>
+        <NavLink
+          className={`${s.navbarBrand} d-md-none ${s.headerSvgFlipColor}`}
+        >
           <i className="fa fa-circle text-primary mr-n-sm" />
           <i className="fa fa-circle text-danger" />
-          &nbsp;
-          sing
-          &nbsp;
+          &nbsp; sing &nbsp;
           <i className="fa fa-circle text-danger mr-n-sm" />
           <i className="fa fa-circle text-primary" />
         </NavLink>
 
         <Nav className="ml-auto">
-          <Dropdown nav isOpen={this.state.notificationsOpen} toggle={this.toggleNotifications} id="basic-nav-dropdown" className={`${s.notificationsMenu}`}>
+          <Dropdown
+            nav
+            isOpen={this.state.notificationsOpen}
+            toggle={this.toggleNotifications}
+            id="basic-nav-dropdown"
+            className={`${s.notificationsMenu}`}
+          >
             <DropdownToggle nav caret className={s.headerSvgFlipColor}>
-            <span className={`${s.avatar} rounded-circle float-left mr-2`}>
-              {avatar ? (
-                <img src={avatar} onError={e => e.target.src = adminDefault} alt="..." title={user && (user.firstName || user.email)} />
-              ) : user && user.role === 'admin' ? (
-                <img src={adminDefault} onError={e => e.target.src = adminDefault} alt="..." title={user && (user.firstName || user.email)} />
-              ) : <span title={user && (user.firstName || user.email)}>{firstUserLetter}</span>
-              }
-            </span>
-              <span className={`small d-sm-down-none ${s.headerTitle} ${this.props.sidebarStatic ? s.adminEmail : ''}`}>{user ? (user.firstName || user.email) : "Philip smith"}</span>
-              <span className="ml-1 circle bg-primary text-white fw-bold d-sm-down-none">13</span>
+              <span className={`${s.avatar} rounded-circle float-left mr-2`}>
+                {avatar ? (
+                  <img
+                    src={avatar}
+                    onError={(e) => (e.target.src = adminDefault)}
+                    alt="..."
+                    title={user && (user.firstName || user.email)}
+                  />
+                ) : user && user.role === "admin" ? (
+                  <img
+                    src={adminDefault}
+                    onError={(e) => (e.target.src = adminDefault)}
+                    alt="..."
+                    title={user && (user.firstName || user.email)}
+                  />
+                ) : (
+                  <span title={user && (user.firstName || user.email)}>
+                    {firstUserLetter}
+                  </span>
+                )}
+              </span>
+              <span
+                className={`small d-sm-down-none ${s.headerTitle} ${
+                  this.props.sidebarStatic ? s.adminEmail : ""
+                }`}
+              >
+                {user ? user.firstName || user.email : "Philip smith"}
+              </span>
+              <span className="ml-1 circle bg-primary text-white fw-bold d-sm-down-none">
+                13
+              </span>
             </DropdownToggle>
-            <DropdownMenu right className={`${s.notificationsWrapper} py-0 animated animated-fast fadeInUp`}>
+            <DropdownMenu
+              right
+              className={`${s.notificationsWrapper} py-0 animated animated-fast fadeInUp`}
+            >
               <Notifications />
             </DropdownMenu>
           </Dropdown>
-          <Dropdown nav isOpen={this.state.menuOpen} toggle={this.toggleMenu} className="tutorial-dropdown pr-4">
+          <Dropdown
+            nav
+            isOpen={this.state.menuOpen}
+            toggle={this.toggleMenu}
+            className="tutorial-dropdown pr-4"
+          >
             <DropdownToggle nav className={`${s.mobileCog}`}>
               <span className={`${s.headerSvgFlipColor}`}>
-                <SettingsIcon/>
+                <FaCog></FaCog>
               </span>
             </DropdownToggle>
-            <DropdownMenu right className={`${s.headerDropdownLinks} super-colors`}>
-              <DropdownItem href="http://demo-flatlogic2.herokuapp.com/sing-app-react/#/app/profile"><span className={s.headerDropdownIcon}><PersonIcon/></span> My Account</DropdownItem>
+
+            <DropdownMenu
+              right
+              className={`${s.headerDropdownLinks} super-colors`}
+            >
+              <DropdownItem href="http://demo-flatlogic2.herokuapp.com/sing-app-react/#/app/profile">
+                <span className={s.headerDropdownIcon}>
+                  <PersonIcon />
+                </span>{" "}
+                My Account
+              </DropdownItem>
               <DropdownItem divider />
-              <DropdownItem href="http://demo-flatlogic2.herokuapp.com/sing-app-react/#/app/extra/calendar"><span className={s.headerDropdownIcon}><CalendarIcon/></span>Calendar</DropdownItem>
-              <DropdownItem href="http://demo-flatlogic2.herokuapp.com/sing-app-react/#/app/inbox"><span className={s.headerDropdownIcon}><EmailIcon/></span>Inbox &nbsp;&nbsp;</DropdownItem>
+              <DropdownItem href="http://demo-flatlogic2.herokuapp.com/sing-app-react/#/app/extra/calendar">
+                <span className={s.headerDropdownIcon}>
+                  <CalendarIcon />
+                </span>
+                Calendar
+              </DropdownItem>
+              <DropdownItem href="http://demo-flatlogic2.herokuapp.com/sing-app-react/#/app/inbox">
+                <span className={s.headerDropdownIcon}>
+                  <EmailIcon />
+                </span>
+                Inbox &nbsp;&nbsp;
+              </DropdownItem>
               <DropdownItem divider />
-              <DropdownItem onClick={this.doLogout}><span className={s.headerDropdownIcon}><PowerIcon/></span> Log Out</DropdownItem>
+              <DropdownItem onClick={this.doLogout}>
+                <span className={s.headerDropdownIcon}>
+                  <PowerIcon />
+                </span>{" "}
+                Log Out
+              </DropdownItem>
             </DropdownMenu>
           </Dropdown>
         </Nav>
@@ -341,4 +431,3 @@ function mapStateToProps(store) {
 }
 
 export default withRouter(connect(mapStateToProps)(Header));
-
